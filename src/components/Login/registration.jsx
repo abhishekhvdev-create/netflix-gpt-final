a2f892b42ff3aa3b8c8f8dd21d5a4d17c740e28f
abhishek <oneminddevelopers@gmail.com>
2026-03-27 19:28:04 +0530

    adding signup page

--- src/components/Login/registration.jsx
+++ src/components/Login/registration.jsx
@@ -1,25 +1,54 @@
-import { useRef } from "react";
-import { EMAIL, NETFLIX_AUTHENTICATION, SIGN_IN } from "../utility/constants";
+import { useRef, useState } from "react";
+import {
+  EMAIL,
+  NAME,
+  NETFLIX_AUTHENTICATION,
+  PASSWORD,
+  SIGN_IN,
+  SIGN_UP,
+  SUBMIT,
+} from "../utility/constants";
 
 export const Registration = () => {
+  const [authentication, setAuthentication] = useState("signin");
   const email = useRef(null);
   const password = useRef(null);
+  const name = useRef(null);
 
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log(email.current.value);
     console.log(password.current.value);
   };
+
+  const handleAuthentication = (message) => {
+    setAuthentication(message);
+  };
   return (
     <div className="absolute top-30 inset-0 font-bold h-30 flex items-center justify-center">
       <form
-        className="flex flex-col my-auto w-100 bg-gray-200 p-10 opacity-95  text-black"
+        className="flex flex-col my-auto w-100 bg-gray-200 p-10 opacity-90  text-black"
         onSubmit={handleSubmit}
       >
         <h1 className="flex text-center justify-center">
           {NETFLIX_AUTHENTICATION}
         </h1>
-        <h2 className="my-3">{SIGN_IN}</h2>
+        <h2 className="my-3">
+          {authentication === "signup" ? SIGN_UP : SIGN_IN}
+        </h2>
+        {authentication === "signup" && (
+          <>
+            <label htmlFor="email">{NAME}</label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              ref={name}
+              placeholder="Enter the Name"
+              className="bg-gray-600 text-white p-2 rounded-xs"
+            />
+          </>
+        )}
         <label htmlFor="email">{EMAIL}</label>
         <input
           type="text"
@@ -30,9 +59,9 @@ export const Registration = () => {
           className="bg-gray-600 text-white p-2 rounded-xs"
         />
 
-        <label htmlFor="password">Password</label>
+        <label htmlFor="password">{PASSWORD}</label>
         <input
-          type="text"
+          type="password"
           name="password"
           id="password"
           ref={password}
@@ -41,8 +70,42 @@ export const Registration = () => {
         />
 
         <button className="text-white font-bold bg-red-500 my-5 p-2 rounded-xs w-full cursor-pointer">
-          Submit
+          {SUBMIT}
         </button>
+
+        {authentication === "signin" && (
+          <span>
+            {"New to Netflix ? Click here to "}
+
+            <a
+              href=""
+              className="underline"
+              onClick={(event) => {
+                event.preventDefault();
+                handleAuthentication("signup");
+              }}
+            >
+              {SIGN_UP}
+            </a>
+          </span>
+        )}
+
+        {authentication === "signup" && (
+          <span>
+            {"Already User ? Click here to "}
+
+            <a
+              href=""
+              className="underline"
+              onClick={(event) => {
+                event.preventDefault();
+                handleAuthentication("signin");
+              }}
+            >
+              {SIGN_IN}
+            </a>
+          </span>
+        )}
       </form>
     </div>
   );
--- src/components/utility/constants.js
+++ src/components/utility/constants.js
@@ -4,4 +4,8 @@ export const NETFLIX_BACKGROUND_IMAGE =
   "https://assets.nflxext.com/ffe/siteui/vlv3/8cc08720-ac1c-4364-bcbd-9495bf0308cd/web/IN-en-20260323-TRIFECTA-perspective_0b8c8e4e-71ee-48bd-8e16-da74f083a838_large.jpg";
 export const NETFLIX_AUTHENTICATION = "Netflix Authentication";
 export const SIGN_IN = "Sign In";
+export const SIGN_UP = "Sign Up";
 export const EMAIL = "Email";
+export const NAME = "Name";
+export const PASSWORD = "Password";
+export const SUBMIT = "Submit";
